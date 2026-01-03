7fbd3126652375c5c98a277c7d8debc3e63b9d38
Osman Anıl Yalçın <osmanilylc@gmail.com>
2026-01-03 13:29:17 +0300

    First push

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.bank.personalfinance {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+
+    opens com.bank.personalfinance to javafx.fxml;
+    exports com.bank.personalfinance;
+}
